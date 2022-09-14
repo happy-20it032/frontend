48cf8bd50dcf764c0c240ccb1bf0e5ce3ad0aa7e
D21IT169 <d21it169@charusat.edu.in>
2022-09-14 13:33:17 +0530

    first commit

---
+++ src/Component/Alert.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { Alert } from "react-bootstrap";
+
+const Alert = (props) => {
+  return (
+      props.alert && <Alert variant={props.alert.type}>{props.alert.message}</Alert>
+      // props.alert && <Alert variant={props.alert.type}>{props.alert.message}</Alert>
+  )
+}
+
+export default Alert
